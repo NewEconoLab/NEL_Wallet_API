6f9fdcec4cfe09eb564028e9619997971c111cdd
tssqzr <1214661466@qq.com>
2019-02-12 10:06:34 +0800

    兼容数据格式不一致情况

--- NEL_Wallet_API/Service/DomainService.cs
+++ NEL_Wallet_API/Service/DomainService.cs
@@ -58,6 +58,8 @@ namespace NEL_Wallet_API.Service
                 string domain = jo["domain"].ToString();
                 jo.Remove("domain");
                 jo.Add("domain", domain + root);
+                if(jo["price"] == null) { jo.Add("price", "0"); }
+                if(jo["type"] == null) { jo.Add("type", ""); }
                 jo.Add("state", jo["type"].ToString() == "NNSfixedSellingLaunched" ? "0901" : "");
                 return jo;
             }).OrderByDescending(p => long.Parse(p["ttl"].ToString())).ToArray();
