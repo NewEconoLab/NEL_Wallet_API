e49ddc72dc35625e37a6995253ca49a05da224f8
tssqzr <1214661466@qq.com>
2019-04-03 17:35:36 +0800

    新增所有权变更后的查询条件

--- NEL_Wallet_API/Service/NNSDomainCreditService.cs
+++ NEL_Wallet_API/Service/NNSDomainCreditService.cs
@@ -1,5 +1,7 @@
-﻿using NEL_Wallet_API.lib;
+﻿using NEL_Wallet_API.Controllers;
+using NEL_Wallet_API.lib;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace NEL_Wallet_API.Service
 {
@@ -12,9 +14,23 @@ namespace NEL_Wallet_API.Service
         public JArray getMappingDomain(string address)
         {
             string findStr = new JObject() { {"address", address } }.ToString();
-            string fieldStr = new JObject() { {"fulldomain", 1 },{ "ttl", 1 },{ "_id",0} }.ToString();
+            string fieldStr = new JObject() { { "fulldomain", 1 }, { "namehash", 1 },{ "ttl", 1 },{ "_id",0} }.ToString();
             var queryRes = mh.GetDataWithField(mongodbConnStr, mongodbDatabase, "nnsDomainCreditState", fieldStr, findStr);
-            return queryRes;
+            if (queryRes == null || queryRes.Count == 0) return new JArray();
+
+            var item = queryRes[0];
+            var namehash = item["namehash"].ToString();
+            findStr = new JObject() { { "namehash", namehash } }.ToString();
+            fieldStr = new JObject() { { "owner",1},{ "TTL",1}, { "_id", 0 } }.ToString();
+            queryRes = mh.GetDataWithField(mongodbConnStr, mongodbDatabase, "domainOwnerCol", fieldStr, findStr);
+            if(queryRes != null && queryRes.Count > 0)
+            {
+                if (queryRes[0]["owner"].ToString() != address || long.Parse(queryRes[0]["TTL"].ToString()) < TimeHelper.GetTimeStamp())
+                {
+                    return new JArray();
+                }
+            }
+            return new JArray { new JObject { { "fulldomain", item["fulldomain"]} } };
         }
     }
 }
